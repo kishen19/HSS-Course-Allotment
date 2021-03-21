e1c7b26e6a9f35a626ab272f2c5491f7a71bbd5b
kishen19 <kishen.gowda@iitgn.ac.in>
2021-03-21 19:42:19 +0530

    added non-interesting courses support to form

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,7 +98,8 @@
   var programs = ["-Select-","B.Tech","M.Tech","MSc","MA","PhD"];
   var courses = ["HS 104 - Foundational Sanskrit", "HS 108 - Japan Studies", "HS 111 - Urdu Script & Poetry", "HS 112 - Urdu Poetry Interpretation", "HS 211 - Exploring India’s Scientific and Technological Heritage","HS 223 - Sanskrit Literature","HS 305 - Perspectives in Psychology", "HS 391 -	Special Topics in HSS: Music Traditions of India", "HS 313 - When You Cannot Experiment: Social Science Methods", "HS 411 - Economic Concepts and Issues in Project Analysis", "HS 425 - Introduction to Archaeology", "HS 491-I - Special Topics in HSS: Indian philosophy through the ages","HS 491-II - Special Topics in HSS: The ‘engineering’ of theatre-making", "HS 491-III - Special Topics in HSS: Storytelling for the Digital Era", "HS 507 - Humanism, Anti-Humanism, and Posthumanism", "HS 510 - Perspectives on Indian Civilization", "HS 512 - Political Thought", "HS 515 - The Politics of the Environment", "HS 518 - Philosophy, Cognition and Psychoanalysis", "HS 519 - Linguistic Anthropology", "HS 610 - A Critical Journey Through Select Thoughts and Theories", "HS 645 - History of India, 1930-1964", "HS 691-I - Special Topics in HSS: Themes in Postcolonialism", "HS 691-II - Special topics in HSS: Applied statistics: Multilevel modeling", "IN 304 - Ancient Indian Technology", "MS 304 - Organizational Behaviour & Human Resource Management (OB & HRM)", "MS 403 - Engineering Entrepreneurship", "MS 491 - Special topics in Management: Marketing Analytics", "HS 691-III - Special Topics in HSS: Abnormal Psychology", "HS 327 (H) - Anthropology, Citizenship and Human Rights (Half Sem course, 2nd Half)", "MS 492 - Special Topics in Management: Finance Management (2 credit course that will run for entire semester, ~2 hours each week)", "MS 491-I - Special Topics in Management: Product Management"];
   var num_courses = courses.length;
-  var temp = '<div id="preference-1" class="post-box remove-padding">\n<div class="row widget-sidebar form-group">\n<p>\nPreference #1 <sup class="required">*</sup>\n <br>\n </p>\n <select class="form-control prefs" id="pref1" name="pref1" placeholder="Your Answer">\n <option value="-Select-">-Select-</option>\n   </select>\n  <p class="error-msg" id="pref1-error"></p>\n  </div>\n</div>'
+  var temp = '<div id="preference-1" class="post-box remove-padding">\n<div class="row widget-sidebar form-group">\n<p>\nPreference #1 <sup class="required">*</sup>\n <br>\n </p>\n <select class="form-control prefs" id="pref1" name="pref1" placeholder="Your Answer">\n <option value="-Select-">-Select-</option>\n   </select>\n  <p class="error-msg" id="pref1-error"></p>\n  </div>\n</div>';
+  var temp2 = '<div id="course-1" class="post-box remove-padding">\n<div class="row widget-sidebar form-group">\n<p>\nCourse #1 <sup class="required">*</sup>\n<br>\n</p>\n<select class="form-control least-prefs" id="course1" name="course1" placeholder="Your Answer">\n<option value="-Select-">-Select-</option>\n</select>\n<p class="error-msg" id="course1-error"></p>\n</div>\n</div>';
 
   $.fn.add_options = function(e){
     $.each(courses,function(index,value){
@@ -307,13 +308,74 @@
     }
   });
 
-  $('.prefs').change(function(){
+  $('#preferences').on('change','.prefs',function(){
     var i = 1;
     while (i<=$('#num-pref').val()){
       $('#pref'+i+'-error').css("display","none");
       $('#pref'+i).css("border-color",border_color);
       $('#pref'+i).css("border-width",border_width);
       $('#pref'+i+'-error').text("");
+      i+=1;
+    }
+  });
+
+  /*--/ Least Preferences /--*/
+
+  $('#num-least-pref').attr("max",num_courses);
+  
+  $('#num-least-pref').change(function(){
+    if($('#num-least-pref').val()>=0 && $('#num-least-pref').val() <= num_courses){
+      $('#num-least-pref-error').css("display","none");
+      $('#num-least-pref-error').text("");
+      $('#num-least-pref').css("border-color",border_color);
+      $('#num-least-pref').css("border-width",border_width);
+      var prefs = $("#least-courses .post-box");
+      if (prefs.length <= $(this).val()){
+        var v = prefs.length+1;
+        while(v<=$(this).val()){
+          $("#least-courses").append(temp2);
+          $("#least-courses .post-box:last").attr("id","course-"+v);
+          $("#course-"+v+" p:first").html("Course #"+v+'<sup class="required">*</sup>');
+          $("#course-"+v+" .form-control").attr("id","course"+v);
+          $("#course-"+v+" .form-control").attr("name","course"+v);
+          $("#course-"+v+" .error-msg").attr("id","course"+v+"-error");
+          $.fn.add_options('#course'+v);
+          v=v+1;
+        };
+      }
+      else{
+        var v = parseInt($(this).val(),10);
+        var w = prefs.length;
+        console.log(v,w);
+        while(w>v){
+          $("#course-"+w).remove();
+          w-=1;
+        };
+      }
+      var i = 1;
+      while (i<=$('#num-least-pref').val()){
+        $('#course'+i+'-error').css("display","none");
+        $('#course'+i).css("border-color",border_color);
+        $('#course'+i).css("border-width",border_width);
+        $('#course'+i+'-error').text("");
+        i+=1;
+      }
+    }
+    else{
+      $('#num-least-pref-error').css("display","block");
+      $('#num-least-pref-error').text("Value must be between 0 and "+num_courses);
+      $('#num-least-pref').css("border-color","#d93025");
+      $('#num-least-pref').css("border-width","3px");
+    }
+  });
+
+  $('#least-courses').on('change','.least-prefs',function(){
+    var i = 1;
+    while (i<=$('#num-least-pref').val()){
+      $('#course'+i+'-error').css("display","none");
+      $('#course'+i).css("border-color",border_color);
+      $('#course'+i).css("border-width",border_width);
+      $('#course'+i+'-error').text("");
       i+=1;
     }
   });
@@ -403,6 +465,14 @@
       $('#minors-error').css("display","block");
       $('#minors-error').text("Invalid Input");
     }
+    else if (!$('#minors-none').is(':checked') && !$('#minors-hss').is(":checked") && !$('#minors-management').is(":checked")){
+      failed=true;
+      $('#minors-none').prop('disabled',false);
+      $('#minors-hss').prop('disabled',false);
+      $('#minors-management').prop('disabled',false);
+      $('#minors-error').css("display","block");
+      $('#minors-error').text("This is a required question");
+    }
 
     /* Validating Number of Preferences */
     if(!$.isNumeric($('#num-pref').val()) || $('#num-pref').val()>num_courses || $('#num-pref').val()<1){
@@ -433,11 +503,65 @@
             $('#pref'+i+'-error').css("display","block");
             $('#pref'+i).css("border-color","#d93025");
             $('#pref'+i).css("border-width","3px");
-            $('#pref'+i+'-error').text("A course can be selected exactly once as a preference");
+            $('#pref'+i+'-error').text("A course can be selected at most once as a preference");
             $('#pref'+j+'-error').css("display","block");
             $('#pref'+j).css("border-color","#d93025");
             $('#pref'+j).css("border-width","3px");
-            $('#pref'+j+'-error').text("A course can be selected exactly once as a preference");
+            $('#pref'+j+'-error').text("A course can be selected at most once as a preference");
+          }
+          j+=1;
+        }
+        i+=1;
+      }
+    }
+
+    /* Validating Number of Least Prefs */
+    if(!$.isNumeric($('#num-least-pref').val()) || $('#num-least-pref').val()>num_courses || $('#num-least-pref').val()<0){
+      failed = true;
+      $('#num-least-pref-error').css("display","block");
+      $('#num-least-pref').css("border-color","#d93025");
+      $('#num-least-pref').css("border-width","3px");
+      $('#num-least-pref-error').text("Value must be between 0 and "+num_courses);
+    }
+
+    /* Validating Least Prefs */
+    if(!failed){
+      var num_prefs = parseInt($('#num-pref').val(),10);
+      var num_least_prefs = parseInt($('#num-least-pref').val(),10);
+      var i=1;
+      while (i<= num_least_prefs-1){
+        var j = i+1;
+        if ($('#course'+i).val()=="-Select-"){
+          failed=true;
+          $('#course'+i+'-error').css("display","block");
+          $('#course'+i).css("border-color","#d93025");
+          $('#course'+i).css("border-width","3px");
+          $('#course'+i+'-error').text("Please select a course");
+          continue;
+        }
+        var k = 1;
+        while (k<=num_prefs){
+          if ($('#course'+i).val()==$('#pref'+k).val()){
+            failed=true;
+            $('#course'+i+'-error').css("display","block");
+            $('#course'+i).css("border-color","#d93025");
+            $('#course'+i).css("border-width","3px");
+            $('#course'+i+'-error').text("You cannot select a course that is already added as a preference.");
+            break;
+          }
+          k+=1;
+        }
+        while (j <= num_least_prefs){
+          if ($('#course'+i).val()==$('#course'+j).val()){
+            failed=true;
+            $('#course'+i+'-error').css("display","block");
+            $('#course'+i).css("border-color","#d93025");
+            $('#course'+i).css("border-width","3px");
+            $('#course'+i+'-error').text("A course can be selected at most once");
+            $('#course'+j+'-error').css("display","block");
+            $('#course'+j).css("border-color","#d93025");
+            $('#course'+j).css("border-width","3px");
+            $('#course'+j+'-error').text("A course can be selected at most once");
           }
           j+=1;
         }
